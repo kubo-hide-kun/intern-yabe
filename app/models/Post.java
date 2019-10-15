c6f3c458732cb689012206f0be39d067e8b9fcb1
kubo-hide-kun <hide44041@gmail.com>
2019-10-15 13:02:05 +0900

    Create Comment Class

--- app/models/Post.java
+++ app/models/Post.java
@@ -17,10 +17,21 @@ public class Post extends Model {
 	@ManyToOne // ← UserClassとの関連を宣言
 	public User author;
 
+	@OneToMany(mappedBy="post",cascade=CascadeType.ALL)
+	public List<Comment> comments;
+
 	public Post(User author, String title, String content) {
+		this.comments = new ArrayList<Comment>();
 		this.author = author;
 		this.title = title;
 		this.content = content;
 		this.postedAt = new Date();
+	}
+
+	public Post addComment(String author,String content) {
+		Comment newComment = new Comment(this, author,content).save();
+		this.comments.add(newComment);
+		this.save();
+		return this;
 	}
 }
--- test/BasicTest.java
+++ test/BasicTest.java
@@ -31,4 +31,53 @@ public class BasicTest extends UnitTest {
     	assertNotNull(firstPost.postedAt);
     }
 
+    @Test
+    public void postComments() {
+        User bob = new User("bob@gmail.com", "secret", "Bob").save();
+        Post bobPost = new Post(bob, "My first post", "Hello world").save();
+
+        new Comment(bobPost, "Jeff", "Nice post").save();
+        new Comment(bobPost, "Tom", "I knew that !").save();
+
+        List<Comment> bobPostComments = Comment.find("byPost", bobPost).fetch();
+
+        assertEquals(2, bobPostComments.size());
+
+        Comment firstComment = bobPostComments.get(0);
+        assertNotNull(firstComment);
+        assertEquals("Jeff", firstComment.author);
+        assertEquals("Nice post", firstComment.content);
+        assertNotNull(firstComment.postedAt);
+
+        Comment secondComment = bobPostComments.get(1);
+        assertNotNull(secondComment);
+        assertEquals("Tom", secondComment.author);
+        assertEquals("I knew that !", secondComment.content);
+        assertNotNull(secondComment.postedAt);
+    }
+
+    @Test
+    public void useTheCommentsRelation() {
+        User bob = new User("bob@gmail.com", "secret", "Bob").save();
+        Post bobPost = new Post(bob, "My first post", "Hello world").save();
+
+        bobPost.addComment("Jeff", "Nice post");
+        bobPost.addComment("Tom", "I knew that !");
+
+        assertEquals(1, User.count());
+        assertEquals(1, Post.count());
+        assertEquals(2, Comment.count());
+
+        bobPost = Post.find("byAuthor", bob).first();
+        assertNotNull(bobPost);
+
+        assertEquals(2, bobPost.comments.size());
+        assertEquals("Jeff", bobPost.comments.get(0).author);
+
+        bobPost.delete();
+
+        assertEquals(1, User.count());
+        assertEquals(0, Post.count());
+        assertEquals(0, Comment.count());
+    }
 }
